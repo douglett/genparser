ec59b4a76ac2ab15582e356d6b3b13864ed3196c
doug@luna <doug@totem9.net>
2023-01-23 21:44:13 +0000

    rules working roughly properly

--- main.cpp
+++ main.cpp
@@ -4,68 +4,78 @@ using namespace std;
 #include "tokenizer.hpp"
 #include "ruleset.hpp"
 
-void test1() {
-	Ruleset rs;
-	rs.setup2();
-	Tokenizer tok;
-	tok.loads("hello world \nhello world");
+// void test1() {
+// 	Ruleset rs;
+// 	rs.setup2();
+// 	Tokenizer tok;
+// 	tok.loads("hello world \nhello world");
 
-	int result = rs.runrule("prog", tok);
-	printf("parse result: %d\n", result);
-}
+// 	int result = rs.runrule("prog", tok);
+// 	printf("parse result: %d\n", result);
+// }
 
-void test2() {
-	Ruleset rs;
-	rs.setup();
-	Tokenizer tok;
-	tok.loads(
-		"\n"
-		"function f\n"
-		"\n"
-		"end function\n"
-	);
+// void test2() {
+// 	Ruleset rs;
+// 	rs.setup();
+// 	Tokenizer tok;
+// 	tok.loads(
+// 		"\n"
+// 		"function f\n"
+// 		"\n"
+// 		"end function\n"
+// 	);
 
-	int result = rs.runrule("prog", tok);
-	printf("parse result: %d\n", result);
-}
+// 	int result = rs.runrule("prog", tok);
+// 	printf("parse result: %d\n", result);
+// }
 
-void test3() {
-	Ruleset rs;
-	rs.addrules({
-		{ "prog", "get", "prog_lines eof" },
-		{ "prog_lines", "mul", "hello_try eol_try" },
-		{ "eol_try", "peek", "eol" },
-		// hello
-		{ "hello_try", "if", "hello_start hello" },
-		{ "hello_start", "peek", "$hello" },
-		{ "hello", "get", "$hello $world endl" }
-	});
-	rs.tok.loads(
-		"hello world\n"
-	);
+// void test3() {
+// 	Ruleset rs;
+// 	rs.addrules({
+// 		{ "prog", "get", "prog_lines eof" },
+// 		{ "prog_lines", "mul", "hello_try eol_try" },
+// 		{ "eol_try", "peek", "eol" },
+// 		// hello
+// 		{ "hello_try", "if", "hello_start hello" },
+// 		{ "hello_start", "peek", "$hello" },
+// 		{ "hello", "get", "$hello $world endl" }
+// 	});
+// 	rs.tok.loads(
+// 		"hello world\n"
+// 	);
 
-	int result = rs.runrule2("prog");
-	printf("parse result: %d\n", result);
-}
+// 	int result = rs.runrule2("prog");
+// 	printf("parse result: %d\n", result);
+// }
 
 void test4() {
 	Ruleset rs;
 	rs.addrules({
-		{ "prog", "require", "proglines eof" },
-		{ "proglines", "mul", "function emptyline" },
+		// { "prog", "", "a | b | c" },
+
+		{ "prog", "require", "*function !eof" },
+		// { "progline", "mul", "function emptyline" },
 		{ "emptyline", "request", "eol" },
+		// function
+		{ "function", "", "$function !ident !$( !$) !endl" },
+		// { "function", "request", "$function function_header function_body function_end" },
+		// { "function_header", "require", "ident $( $) endl" },
+		// { "function_body", "mul", "emptyline" },
+		// { "function_end", "require", "$end $function endl" },
 	});
 	rs.tok.loads(
-		"hello world\n"
+		"function foo()"
+		// "end function"
 	);
 
-	int result = rs.runrule2("prog");
+	int result = rs.runrule("prog");
 	printf("parse result: %d\n", result);
 }
 
 int main() {
 	// test1();
 	// test2();
-	// test4();
-	test3();
+	// test3();
+	test4();
+	printf("%d\n", isident("foo"));
 }
--- ruleset.hpp
+++ ruleset.hpp
@@ -36,133 +36,100 @@ struct Ruleset {
 			addrule(rs[0], rs[1], rs[2]);
 	}
 
-	int runrule(const string& name, Tokenizer& tok) {
+	int runrule(const string& name) {
 		int pos = tok.pos;
-		#define gettok()  (tok.get(), printf("found token: [%s]\n", name.c_str()))
-		#define getrule() (printf("found rule: [%s]\n", name.c_str()), 1)
-		#define unget()   (tok.pos = pos, 0)
+		#define gettok()    ( tok.get(), printf("found token: [%s]\n", name.c_str()), 1 )
+		#define getrule()   ( printf("found rule: [%s]\n", name.c_str()), 1 )
+		#define unget()     ( tok.pos = pos, 0 )
+		// #define errtok()    ( throw parse_error(string("error at token: ") + tok.peek()), 0 )
+		#define reqerror()  ( throw parse_error(string("rule required: ") + name + ", got [" + tok.peek() + "]"), 0 )
 		// built in rules
-		// special case - literal rule
-		if      (name.length() >= 2 && name[0] == '$') return tok.peek() == name.substr(1) ? (gettok(), 1) : 0;
-		else if (name == "eol")  return tok.peek() == tok.TOK_EOL ? (gettok(), 1) : 0;
-		else if (name == "eof")  return tok.peek() == tok.TOK_EOF ? 1 : 0;
-		else if (name == "endl") return runrule("eol", tok) || runrule("eof", tok);
-		// user defined rules
+		if      (name.length() < 2)  throw parse_error(string("bad rule [") + name + "]");
+		// rule modifiers
+		else if (name[0] == '$')     return tok.peek() == name.substr(1) ? gettok() : 0;  // string literal
+		else if (name[0] == '!')     return runrule(name.substr(1)) ? 1 : reqerror();  // run rule or error
+		else if (name[0] == '?')     return runrule(name.substr(1)), 1;  // rule optional
+		else if (name[0] == '*')     { while (runrule(name.substr(1))) ; return 1; }
+		// literal rules
+		else if (name == "eol")      return tok.peek() == tok.TOK_EOL ? gettok() : 0;
+		else if (name == "eof")      return tok.peek() == tok.TOK_EOF ? 1 : 0;
+		else if (name == "endl")     return tok.peek() == tok.TOK_EOL || tok.peek() == tok.TOK_EOF ? gettok() : 0;
+		else if (name == "ident")    return isident(tok.peek()) ? gettok() : 0;
 		// check rule existance
-		if (rules.count(name) == 0) throw parse_error("missing rule: " + name);
-		const auto& rule = rules[name];
+		else if (!rules.count(name)) throw parse_error("missing rule: " + name);
+		// run rules in order
+		for (const auto& sr : rules[name].subrules)
+			if (!runrule(sr))
+				return unget();
+		return getrule();
+
+
 		// run rule by rule type
-		if (rule.type == "get") {
-			for (auto& sr : rule.subrules)
-				if (!runrule(sr, tok)) return unget();
-			return getrule();
-		}
-		else if (rule.type == "any") {
-			for (auto& sr : rule.subrules)
-				if (runrule(sr, tok)) return getrule();
-			return unget();
-		}
-		else if (rule.type == "mul") {
-			int found = 1;
-			while (found) {
-				found = 0;
-				for (auto& sr : rule.subrules)
-					if (runrule(sr, tok)) found = 1;
-			}
-			return getrule();
-		}
-		else if (rule.type == "if") {
-			if      (rule.subrules.size() != 2) throw parse_error("rule-type if requires 2 subrules, found " + to_string(rule.subrules.size()));
-			else if (runrule(rule.subrules[0], tok)) return unget(), runrule(rule.subrules[1], tok);
-			else    return unget();
-		}
-		throw parse_error("unknown rule-type: " + rule.type);
-	}
+		// if (rule.type == "request") {
+		// 	for (auto& sr : rule.subrules)
+		// 		if (!runrule(sr)) 
+		// 			return unget();
+		// 	return getrule();
+		// }
+		// else if (rule.type == "require") {
+		// 	for (auto& sr : rule.subrules)
+		// 		if (!runrule(sr)) 
+		// 			return errtok();
+		// 	return getrule();
+		// }
+		// else if (rule.type == "mul") {
+		// 	while (true) {
+		// 		for (auto& sr : rule.subrules)
+		// 			if (runrule(sr)) 
+		// 				continue;
+		// 		return 1;
+		// 	}
+		// }
 
-	// int tokget() {
-	// 	tok.get();
-	// 	printf("found token: [%s]\n", name.c_str());
-	// 	return 1;
-	// }
 
-	// int tokerr() {
-	// 	throw parse_error("error");
-	// }
-
-	int runrule2(const string& name) {
-		#define gettok2()  (tok.get(), printf("found token: [%s]\n", name.c_str()), 1)
-		#define getrule2() (printf("found rule: [%s]\n", name.c_str()), 1)
-		#define errtok2()  (throw parse_error(string("error at token: ") + tok.peek()))
-		// built in rules
-		// special case - literal rule
-		if      (name.length() >= 2 && name[0] == '$') return tok.peek() == name.substr(1) ? gettok2() : errtok2();
-		else if (name == "eol")  return tok.peek() == tok.TOK_EOL ? gettok2() : errtok2();
-		else if (name == "eof")  return tok.peek() == tok.TOK_EOF ? 1 : errtok2();
-		else if (name == "endl") return tok.peek() == tok.TOK_EOL || tok.peek() == tok.TOK_EOF ? gettok2() : errtok2();
-		// check rule existance
-		if (rules.count(name) == 0) throw parse_error("missing rule: " + name);
-		const auto& rule = rules[name];
-		// run rule by rule type
-		if (rule.type == "get") {
-			for (auto& sr : rule.subrules)
-				runrule2(sr);
-			return getrule2();
-		}
-		else if (rule.type == "peek") {
-			int pos = tok.pos;
-			// string rname;
-			try {
-				for (auto& sr : rule.subrules)
-					// runrule2(rname = sr);
-					runrule2(sr);
-			}
-			catch (parse_error& e) {
-				// printf("error peeking rule [%s]: %s\n", rname.c_str(), e.what());
-				printf("error peeking rule [%s]: %s\n", rule.name.c_str(), e.what());
-				tok.pos = pos;
-				return 0;
-			}
-			printf("peek rule [%s] ok\n", rule.name.c_str());
-			tok.pos = pos;
-			return 1;
-		}
-		else if (rule.type == "mul") {
-			int found = 1;
-			while (found) {
-				found = 0;
-				for (auto& sr : rule.subrules)
-					if (runrule2(sr)) found = 1;
-			}
-			return 1;
-		}
-		else if (rule.type == "any") {
-			for (auto& sr : rule.subrules)
-				if (runrule2(sr)) return 1;
-			return 0;
-		}
-		else if (rule.type == "if") {
-			if      (rule.subrules.size() != 2) throw parse_error("rule-type if requires 2 subrules, found " + to_string(rule.subrules.size()));
-			else if (runrule2(rule.subrules[0])) return runrule2(rule.subrules[1]), 1;
-			else    return 0;
-		}
+		// if (rule.type == "get") {
+		// 	for (auto& sr : rule.subrules)
+		// 		runrule2(sr);
+		// 	return getrule();
+		// }
+		// else if (rule.type == "peek") {
+		// 	int pos = tok.pos;
+		// 	// string rname;
+		// 	try {
+		// 		for (auto& sr : rule.subrules)
+		// 			// runrule2(rname = sr);
+		// 			runrule2(sr);
+		// 	}
+		// 	catch (parse_error& e) {
+		// 		// printf("error peeking rule [%s]: %s\n", rname.c_str(), e.what());
+		// 		printf("error peeking rule [%s]: %s\n", rule.name.c_str(), e.what());
+		// 		tok.pos = pos;
+		// 		return 0;
+		// 	}
+		// 	printf("peek rule [%s] ok\n", rule.name.c_str());
+		// 	tok.pos = pos;
+		// 	return 1;
+		// }
+		// else if (rule.type == "mul") {
+		// 	int found = 1;
+		// 	while (found) {
+		// 		found = 0;
+		// 		for (auto& sr : rule.subrules)
+		// 			if (runrule2(sr)) found = 1;
+		// 	}
+		// 	return 1;
+		// }
+		// else if (rule.type == "any") {
+		// 	for (auto& sr : rule.subrules)
+		// 		if (runrule2(sr)) return 1;
+		// 	return 0;
+		// }
+		// else if (rule.type == "if") {
+		// 	if      (rule.subrules.size() != 2) throw parse_error("rule-type if requires 2 subrules, found " + to_string(rule.subrules.size()));
+		// 	else if (runrule2(rule.subrules[0])) return runrule2(rule.subrules[1]), 1;
+		// 	else    return 0;
+		// }
 		// rule type not found
-		throw parse_error("unknown rule-type: " + rule.type);
-	}
-
-	void setup() {
-		addrule("prog", "mul", "function emptyline");
-		addrule("emptyline", "get", "eol");
-		addrule("function", "if", "function_start function_main");
-		addrule("function_start", "get", "$function");
-		addrule("function_main", "get", "$function $( $) eol function_body $end $function");
-		addrule("function_body", "mul", "emptyline");
-
-		showrules();
-	}
-	
-	void setup2() {
-		addrule("prog", "mul", "hello eol");
-		addrule("hello", "get", "$hello $world");
-		showrules();
+		throw parse_error("unknown rule-type: " + rules[name].type);
 	}
 };
